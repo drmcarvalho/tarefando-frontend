91bf7cd9892a394daba48cc739ebe0e9b47449f2
drmcarvalho <furiano.k@gmail.com>
2025-09-29 10:31:43 -0300

    scroll

--- src/tarefando.ts
+++ src/tarefando.ts
@@ -109,6 +109,7 @@ class MyTaskComponent extends LitElement {
             max-width: 800px;
             margin: 0 auto;
             padding: 20px;
+            scroll;
         }
 
         /* Estilos para modo agrupado */
